f4753314fdd9b528a62f99912cc0415f38d7f6c4
Hugo <albrechthugo4@gmail.com>
2021-01-29 17:08:11 -0300

    🚀 initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Column(
+      children: [
+        Text('Bem vindo ao Bytebank!', textDirection: TextDirection.ltr)
+      ],
+    )
+  );
+}
